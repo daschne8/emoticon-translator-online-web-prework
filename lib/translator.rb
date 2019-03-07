516dd172799eeca2445c2809f744309350ffa65d
Daniel Schneider <daschne8@gmail.com>
2019-03-07 17:37:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,14 @@ require "yaml"
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
-
+  get_meaning = {}
+  get_emoticon = {}
   new_hash = {
     get_meaning:get_meaning,
     get_emoticon:get_emoticon
   }
-  binding.pry
-  puts "un-bind"
+  # binding.pry
+  # puts "un-bind"
   return new_hash
 end
 
